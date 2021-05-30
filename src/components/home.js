2e1acaf95c1539139d4dfef2cbcdfdcaea38bf95
Tiiso Theophilus Senosha <theotiiso@gmail.com>
2021-05-30 03:16:36 +0200

    Updated components and imported them to the App.js

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link, Redirect} from 'react-router-dom';
+
+export default class Login extends Component {
+    
+    render() {
+        return (
+            
+        );
+    }
+}

671964ed6ba1baab709cc073fa13e6f09ff42c64
webreise <52330824+webreise@users.noreply.github.com>
2021-05-30 02:09:33 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // This binding is necessary to make `this` work in the callback
+    this.handlelistenClick = this.handlelistenClick.bind(this);
+  }
+
+  handlelistenClick() {
+
+      var axios = require("axios").default;
+
+      var options = {
+        method: 'GET',
+        url: 'https://free-news.p.rapidapi.com/v1/search',
+        params: {q: 'Covid-19', lang: 'en'},
+        headers: {
+          'x-rapidapi-key': '8447133026mshca7cb2f05a4409bp1a0bf4jsn957c820e10b5',
+          'x-rapidapi-host': 'free-news.p.rapidapi.com'
+        }
+      };
+
+      axios.request(options).then(function (response) {
+          var msg = new SpeechSynthesisUtterance();
+          msg.text = response.data.articles[1].title + response.data.articles[1].summary;
+          window.speechSynthesis.speak(msg);
+          
+
+      }).catch(function (error) {
+        console.error(error);
+      });
+
+  }
+
+  render() {
+    return (
+      <button onClick={this.handlelistenClick}>
+        Listen
+      </button>
+    );
+  }
 }
 
 export default App;
